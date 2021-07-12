f3b92d901b2bbdd15e42a2510b118e39e4c4f2e2
zhaoyanz405 <zhaoyanz405@gmail.com>
2021-07-12 16:24:15 +0800

    feat: add main app to restart docker container lakeeee

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/docker/lakeeee')
+def restart_lakeeee():
+    os.system("docker restart lakeeee")
+    return 0
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
